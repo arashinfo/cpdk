c4399e775cec37b22643ecb479190e459fb56056
arashdeep singh <arash@gmail.com>
2019-03-17 02:43:34 +0530

    fix test

--- test.php
+++ test.php
@@ -1,2 +1,14 @@
+
 <?php
-//
+require_once './vendor/autoload.php';
+
+use Arashinfo\Cpdk;
+
+$server = new Cpdk\ServerInfo();
+
+
+
+echo $server->getHttpHost();
+echo $server->getScriptName();
+echo $server->getRemoteAddr();
+echo $server->getRequestMethod();
